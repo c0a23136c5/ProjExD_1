7e8ecf327cb9a3c60190d2c97fd8ebb9c584da45
masu_p <c0a23136c5@edu.teu.ac.jp>
2024-06-11 17:07:35 +0900

    こうかとんがキーを押して動けるようにする

--- pygame_image.py
+++ pygame_image.py
@@ -15,6 +15,8 @@ def main():
     kk_img = pg.transform.flip(kk_img, True, False)
     bg_ing2 = pg.transform.flip(bg_ing2, True, False)
     tmr = 0
+    kk_rect = kk_img.get_rect()
+    kk_rect.center = 300, 200
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
@@ -23,9 +25,17 @@ def main():
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_ing2, [-x + 1600, 0])
         screen.blit(bg_img, [-x + 3200, 0])
-        screen.blit(bg_ing2, [-x + 4800, 0])        
-        kk_rect = kk_img.get_rect()#こうかとんRectの抽出
-        kk_rect.center = 300, 200
+        screen.blit(bg_ing2, [-x + 4800, 0])  
+
+        key_lst = pg.key.get_pressed()#全キーの押込み状態を取得
+        if key_lst[pg.K_UP]:
+            kk_rect.move_ip((0, -1))
+        if key_lst[pg.K_DOWN]:
+            kk_rect.move_ip((0, 1))
+        if key_lst[pg.K_LEFT]:
+            kk_rect.move_ip((-1, 0))
+        if key_lst[pg.K_RIGHT]:
+            kk_rect.move_ip(1, 0)   
         screen.blit(kk_img, kk_rect)
         pg.display.update()
         tmr += 1        
