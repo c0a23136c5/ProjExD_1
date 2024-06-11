4ee688d838095e32b4af0f11a71f46bb90f19481
masu_p <c0a23136c5@edu.teu.ac.jp>
2024-06-11 16:38:39 +0900

    背景のワープ対策

--- pygame_image.py
+++ pygame_image.py
@@ -11,14 +11,19 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.image.load("fig/3.png")
+    bg_ing2 = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.flip(kk_img, True, False)
+    bg_ing2 = pg.transform.flip(bg_ing2, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr % 800
+        x = tmr % 3200
         screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_ing2, [-x + 1600, 0])
+        screen.blit(bg_img, [-x + 3200, 0])
+        screen.blit(bg_ing2, [-x + 4800, 0])        
         kk_rect = kk_img.get_rect()#こうかとんRectの抽出
         kk_rect.center = 300, 200
         screen.blit(kk_img, kk_rect)
